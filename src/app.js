78dbad20dab0390a8f60dc0d8cffa7e2627821e2
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-06-12 23:31:20 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let card = document.getElementById("thecard");
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const cardContent = [2, 3, 4, 5, 6, 7, 8, 9, "jack", "queen", "king", "ace"];
+card.addEventListener("mouseenter", function() {
+  let text = document.getElementById("text");
+  let suitRandom = suits[Math.floor(Math.random() * suits.length)];
+  let cardRandom = cardContent[Math.floor(Math.random() * cardContent.length)];
+  let face = document.getElementById("cardface");
+  if (
+    cardRandom === "jack" ||
+    cardRandom === "queen" ||
+    cardRandom === "king"
+  ) {
+    face.src = `/src/SVG-cards-1.3/${cardRandom}_of_${suitRandom}2.svg`;
+  } else {
+    face.src = `/src/SVG-cards-1.3/${cardRandom}_of_${suitRandom}.svg`;
+  }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  text.innerHTML = `Your card is the <span id = "color">${cardRandom}</span> of <span id = "color">${suitRandom}</span>`;
+  let color = document.querySelectorAll("#color");
+  if (suitRandom === "hearts" || suitRandom === "diamonds") {
+    color[0].style.color = "red";
+    color[1].style.color = "red";
+  } else {
+    color[0].style.color = "black";
+    color[1].style.color = "black";
+  }
+});
